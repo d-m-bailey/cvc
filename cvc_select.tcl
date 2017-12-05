4a832a0e77df6de4919f8e469d268f21ee52031e
D. Mitch Bailey <d.mitch.bailey@gmail.com>
2017-12-05 11:08:20 +0900

    cvc_select changes
    - attempt to handle empty selection
    - use partial path for subcircuit debugging
    - changed highlight color for new spicevision
    - removed unnecessary character conversion
    - redisplay search pane when loading

--- cvc_select.tcl
+++ cvc_select.tcl
@@ -1,60 +1,66 @@
 proc load_selection {} {
-        global gClipboardSource
-        if { ! [info exists gClipboardSource] } {
-                set gClipboardSource clipboard
-        }
-        set myClipboard [open "|xclip -o -selection $gClipboardSource" r]
-        gets $myClipboard mySelection
-        Gui:Print $mySelection
+	global gClipboardSource
+	if { ! [info exists gClipboardSource] } {
+		set gClipboardSource clipboard
+	}
+	set myClipboard [open "|xclip -o -selection $gClipboardSource" r]
+	if { "$myClipboard" eq "" } {
+		set mySelelction ""
+	} else {
+		gets $myClipboard mySelection
+	}
+	Gui:Print $mySelection
 
-        set visu [Gui:GetMainVisualizer]
+	set visu [Gui:GetMainVisualizer]
 
-        set db [Gui:GetDataBase $visu]
-        set topoid [Gui:GetCurrentModule $visu]
-        if { $topoid == "" } {
-                $db foreach top topoid {
-                        break
-                }
-        }
-        set topcell [$db oid root $topoid]
+	set db [Gui:GetDataBase $visu]
+	set topoid [Gui:GetCurrentModule $visu]
+	if { $topoid == "" } {
+		$db foreach top topoid {
+			break
+		}
+	}
+	set topcell [$db oid root $topoid]
+	set path [$db oid path $topoid]
 
-        set     oidList {}
-        regsub {\([^\)]*\)[     ]*$} $mySelection "" mySelection
-        regsub -all {\([^\)]*\)/} $mySelection " " mySelection
-        regsub -all {==} $mySelection "--" mySelection
-        regsub {^/} $mySelection "" mySelection
-        regsub -all {/} $mySelection "-" mySelection
-        regsub -all {\[} $mySelection "<" mySelection
-        regsub -all {\]} $mySelection ">" mySelection
-        #Gui:Print $mySelection
-        lappend oidList "inst $topcell $mySelection"
-        Gui:AppendCone $visu $oidList
+	set oidList {}
+	regsub {\([^\)]*\)[     ]*$} $mySelection "" mySelection
+	regsub -all {\([^\)]*\)/} $mySelection " " mySelection
+	regsub -all {==} $mySelection "--" mySelection
+	regsub {^/} $mySelection "" mySelection
+	#regsub -all {/} $mySelection "-" mySelection
+	#regsub -all {\[} $mySelection "<" mySelection
+	#regsub -all {\]} $mySelection ">" mySelection
+	#Gui:Print $mySelection
+	lappend oidList "inst $topcell $path $mySelection"
+	Gui:AppendCone $visu $oidList
 
-        set col 14
+	set col 12
 
-        #set db [Gui:GetDataBase $visu]
-        foreach oid $oidList {
-                $db flathilight $oid set $col
-        }
-        Gui:HighlightChanged
+	#set db [Gui:GetDataBase $visu]
+	foreach oid $oidList {
+		$db flathilight $oid set $col
+	}
+	Gui:HighlightChanged
 
-        set     oidList {}
-        regsub { [^ ]*$} $mySelection "" mySelection
-        #Gui:Print $mySelection
-        lappend oidList "inst $topcell $mySelection"
-        Gui:AppendCone $visu $oidList
+	set oidList {}
+	regsub { [^ ]*$} $mySelection "" mySelection
+	#Gui:Print $mySelection
+	lappend oidList "inst $topcell $path $mySelection"
+	Gui:AppendCone $visu $oidList
 
-        Gui:ActivateTab $visu Cone
+	Gui:ActivateTab $visu Search&Cone
 }
+
 proc toggle_selection {} {
-        global gClipboardSource
-        if { [info exists gClipboardSource] && "$gClipboardSource" eq "clipboard" } {
-                set gClipboardSource primary
-                Gui:Print "Clipboard selection set to X"
-        } else {
-                set gClipboardSource clipboard
-                Gui:Print "Clipboard selection set to Windows"
-        }
+	global gClipboardSource
+	if { [info exists gClipboardSource] && "$gClipboardSource" eq "clipboard" } {
+		set gClipboardSource primary
+		Gui:Print "Clipboard selection set to X"
+	} else {
+		set gClipboardSource clipboard
+		Gui:Print "Clipboard selection set to Windows"
+	}
 }
 
 bind . <Control-k> {load_selection}
